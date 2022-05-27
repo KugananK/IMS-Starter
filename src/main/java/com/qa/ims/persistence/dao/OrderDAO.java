2c1050e6f050ba786c484e451773124e950f23b4
Kajan Kugananthajothy <kajankugananthajothy@gmail.com>
2022-05-27 13:46:24 +0100

    up to date main classes

---
+++ src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
@@ -0,0 +1,158 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.utils.DBUtils;
+
+public class OrderDAO implements Dao<Order> {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	private ItemDAO itemDAO;
+
+	public OrderDAO(ItemDAO itemDAO) {
+		super();
+		this.itemDAO = itemDAO;
+	}
+
+	@Override
+	public Order modelFromResultSet(ResultSet resultSet) throws SQLException {
+		Long id = resultSet.getLong("order_id");
+		Long customerID = resultSet.getLong("customer_id");
+		List<Item> products = readItems(id);
+
+		return new Order(id, customerID, products);
+	}
+
+	public List<Item> readItems(Long order_id) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection
+						.prepareStatement("SELECT * FROM joining WHERE order_id = " + order_id);) {
+			statement.setLong(1, order_id);
+			try (ResultSet resultSet = statement.executeQuery();) {
+				List<Long> itemIds = new ArrayList<>();
+				while (resultSet.next()) {
+					itemIds.add(resultSet.getLong("item_id"));
+
+				}
+				List<Item> itemList = new ArrayList<>();
+				for (int i = 0; i < itemIds.size(); i++) {
+					Item readItem = itemDAO.read(itemIds.get(i));
+					itemList.add(readItem);
+				}
+				return itemList;
+
+			}
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
+
+	}
+
+	@Override
+	public List<Order> readAll() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM orders");) {
+			List<Order> orders = new ArrayList<>();
+			while (resultSet.next()) {
+				orders.add(modelFromResultSet(resultSet));
+			}
+			return orders;
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
+	}
+
+	public Order readLatest() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM orders ORDER BY order_id DESC LIMIT 1");) {
+			resultSet.next();
+			return modelFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	@Override
+	public Order create(Order order) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection
+						.prepareStatement("INSERT INTO orders(customer_id) VALUES (?)");) {
+			statement.setLong(1, order.getCustomer_id());
+			statement.executeUpdate();
+			return readLatest();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+
+	}
+
+	@Override
+	public Order read(Long id) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection.prepareStatement("SELECT * FROM orders WHERE order_id = ?");) {
+			statement.setLong(1, id);
+			try (ResultSet resultSet = statement.executeQuery();) {
+				resultSet.next();
+				return modelFromResultSet(resultSet);
+			}
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	@Override
+	public Order update(Order order) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection
+						.prepareStatement("UPDATE orders SET customer_id = ? WHERE  order_id = ?");) {
+			statement.setLong(1, order.getCustomer_id());
+			statement.setLong(2, order.getOrder_id());
+			statement.executeUpdate();
+			return read(order.getOrder_id());
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+
+	}
+
+	@Override
+	public int delete(long id) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection.prepareStatement("DELETE FROM orders WHERE order_id = ?");) {
+			statement.setLong(1, id);
+			return statement.executeUpdate();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return 0;
+
+	}
+
+}
---
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -0,0 +1,126 @@
+package com.qa.ims.persistence.domain;
+
+import java.util.List;
+
+public class Order {
+
+	private long order_id;
+	private long customer_id;
+	private List<Item> items;
+	private double orderCost;
+
+	public Order(long order_id, long customer_id, List<Item> items, double orderCost) {
+		super();
+		this.order_id = order_id;
+		this.customer_id = customer_id;
+		this.items = items;
+		this.orderCost = orderCost;
+	}
+
+	public Order(long order_id, long customer_id, List<Item> items) {
+		super();
+		this.order_id = order_id;
+		this.customer_id = customer_id;
+		this.items = items;
+	}
+
+	public Order(long order_id, long customer_id) {
+		super();
+		this.order_id = order_id;
+		this.customer_id = customer_id;
+	}
+
+	public Order(long customer_id, List<Item> items, double orderCost) {
+		super();
+		this.customer_id = customer_id;
+		this.items = items;
+		this.orderCost = orderCost;
+	}
+
+	public Order(long customer_id) {
+		super();
+		this.customer_id = customer_id;
+	}
+
+	public Order(long customer_id, List<Item> items) {
+		super();
+		this.customer_id = customer_id;
+		this.items = items;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [order_id=" + order_id + ", customer_id=" + customer_id + ", items=" + items + ", orderCost="
+				+ orderCost + "]";
+	}
+
+	public long getOrder_id() {
+		return order_id;
+	}
+
+	public void setOrder_id(long order_id) {
+		this.order_id = order_id;
+	}
+
+	public long getCustomer_id() {
+		return customer_id;
+	}
+
+	public void setCustomer_id(long customer_id) {
+		this.customer_id = customer_id;
+	}
+
+	public List<Item> getItems() {
+		return items;
+	}
+
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
+
+	public double getOrderCost() {
+		return orderCost;
+	}
+
+	public void setOrderCost(double orderCost) {
+		this.orderCost = orderCost;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (customer_id ^ (customer_id >>> 32));
+		result = prime * result + ((items == null) ? 0 : items.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(orderCost);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + (int) (order_id ^ (order_id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (customer_id != other.customer_id)
+			return false;
+		if (items == null) {
+			if (other.items != null)
+				return false;
+		} else if (!items.equals(other.items))
+			return false;
+		if (Double.doubleToLongBits(orderCost) != Double.doubleToLongBits(other.orderCost))
+			return false;
+		if (order_id != other.order_id)
+			return false;
+		return true;
+	}
+	
+
+}
